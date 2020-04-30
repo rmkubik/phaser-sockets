45e26a4dae78d86455717314a02d51dc112d4c1d
Ryan Kubik <rmkubik@me.com>
2020-04-29 23:29:44 -0700

    Configure port via cli arg

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,6 @@ io.on("connection", function (socket) {
   });
 });
 
-http.listen(3000, function () {
-  console.log("listening on *:3000");
+http.listen(process.argv[2] || 3000, function () {
+  console.log(`listening on *:${process.argv[2] || 3000}`);
 });
